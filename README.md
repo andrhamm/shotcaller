72ba46e608ccf5deae61828e2769a2b51ae72229
Andrew Hammond <andrhamm@users.noreply.github.com>
2021-04-14 16:19:01 -0400

    Add notification test script

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ Polls `CVS.com` for COVID-19 vaccine appointment availability, triggers macOS no
 
 Pass a state abbreviation to the commands (examples show `vt`).
 
-Optionally install `terminal-notifier` for system notifications.
-
-    brew install terminal-notifier
-
 With NPM:
 
     npm install -g shotcaller
@@ -25,13 +21,29 @@ Or, from source:
     git clone git@github.com:andrhamm/shotcaller.git
     cd shotcaller
     yarn build
-    yarn cvs vt
-    // or
+    yarn shotcaller vt
+    # or
     ./bin/shotcaller vt
+
+## Notifications (optional)
+
+Optionally install `terminal-notifier` for system notifications.
+
+    brew install terminal-notifier
+
+Tip: On macOS, go into `System Preferences -> Notifications`, find the `terminal-notifier` app in the left scroll view, and set the alert style to `Alerts`. This will make notifications sticky until dismissed so you don't miss them.
+
+Once you have the dependencies set up, test the notifications by passing `test` in place of the state abbreviation:
+
+    shotcaller test
+    # or
+    yarn notify
 
 ---
 
 ## TODO
 
+- SMS/email notification, or other AFK alert medium
 - Add support for Walgreens and other providers
 - Figure out why `node-notifier` package doesn't work (plist error)
+- Improve how the npm module is packaged/published?
--- src/cvs.js
+++ src/cvs.js
@@ -2,21 +2,12 @@ import axios from "axios";
 import { execSync } from "child_process";
 import {
   logDebug,
+  logError,
   logErrorAndFail,
   logInfo,
   logSuccess,
   sleepMinutes,
 } from "./utils";
-
-let terminalNotifierPath;
-try {
-  terminalNotifierPath = execSync(`which terminal-notifier`).toString().trim();
-  logDebug(`Found terminal-notifier at ${terminalNotifierPath}`);
-} catch (error) {
-  logError(
-    `Must install terminal-notifier dependency for notifications (brew install terminal-notifier)`
-  );
-}
 
 const { [2]: stateInput } = process.argv;
 if (!stateInput) {
@@ -24,21 +15,42 @@ if (!stateInput) {
 }
 const state = stateInput.toUpperCase();
 
+let terminalNotifierPath;
+try {
+  terminalNotifierPath = execSync(`which terminal-notifier`).toString().trim();
+  logDebug(`Found terminal-notifier at ${terminalNotifierPath}`);
+} catch (error) {
+  const errorFn = state === "TEST" ? logErrorAndFail : logError;
+  errorFn(
+    `Must install terminal-notifier dependency for notifications (brew install terminal-notifier)`
+  );
+}
+
+const referer =
+  "https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-link2-coronavirus-vaccine";
+const request = {
+  url: `https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.${state}.json?vaccineinfo`,
+  method: "get",
+  headers: {
+    Accept: "*/*",
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0",
+    "Accept-Language": "en-US,en;q=0.5",
+    Referer: referer,
+  },
+  responseType: "json",
+};
+
+if (state === "TEST" && terminalNotifierPath) {
+  // Send a test notification
+  const notifyStatus = notify("FOOBAR", referer);
+  if (notifyStatus === 0) {
+    logSuccess(`No problems detected!`);
+  }
+  process.exit(notifyStatus);
+}
+
 async function run() {
-  const referer =
-    "https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-link2-coronavirus-vaccine";
-  const request = {
-    url: `https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.${state}.json?vaccineinfo`,
-    method: "get",
-    headers: {
-      Accept: "*/*",
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0",
-      "Accept-Language": "en-US,en;q=0.5",
-      Referer: referer,
-    },
-    responseType: "json",
-  };
   const response = await axios(request);
 
   const {
@@ -81,9 +93,7 @@ async function run() {
       .join(", ");
 
     if (terminalNotifierPath) {
-      execSync(
-        `terminal-notifier -title "CVS has availability!" -subtitle "In ${escapedAvailability}" -message "Click to open page in browser now!" -open "${referer}" -timeout "10" -json "true"`
-      );
+      notify(escapedAvailability, referer);
     }
     logSuccess(
       `CVS has availability! In ${escapedAvailability}. Go now! ${referer}`
@@ -93,11 +103,26 @@ async function run() {
   }
 }
 
+function notify(escapedAvailability, callToActionUrl) {
+  try {
+    execSync(
+      `terminal-notifier -title "CVS has availability!" -subtitle "In ${escapedAvailability}" -message "Click to open page in browser now!" -open "${callToActionUrl}" -sound "Glass" -ignoreDnD`
+    );
+    return 0;
+  } catch (error) {
+    logError(
+      `Failed to send notification (status=${error.status} ${error.message})`
+    );
+    return error.status;
+  }
+}
+
 function poll() {
   logInfo(`Polling in 5 minute intervals...`);
   return new Promise((resolve, reject) =>
     run().then(resolve).catch(reject)
   ).finally(() => {
+    // Note: please be respectful and keep a reasonable interval
     logDebug(`Sleeping 5 minutes...`);
     sleepMinutes(5).then(poll);
   });
