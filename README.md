def9dc7e221de3772548def7f78b3771b3b4a1b5
Andrew Hammond <andrhamm@users.noreply.github.com>
2021-04-14 21:58:23 -0400

    Add SMS support via Twilio (requires free trial account)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # shotcaller
 
-Polls `CVS.com` for COVID-19 vaccine appointment availability, triggers macOS notification when availability is found.
+Polls `CVS.com` for COVID-19 vaccine appointment availability, triggers OS notifications and/or SMS notifications when availability is found.
 
 ## Installation and Usage
 
@@ -23,12 +23,12 @@ Or, from source:
     yarn install
     yarn build
     yarn shotcaller vt
-    # or
-    ./bin/shotcaller vt
 
 ## Notifications (optional)
 
-Optionally install `terminal-notifier` for system notifications.
+### System Notifications
+
+Optionally install `terminal-notifier` for system notifications. These instructions only cover macOS usage but other platforms are supported, check out the [docs](https://github.com/julienXX/terminal-notifier).
 
     brew install terminal-notifier
 
@@ -37,14 +37,48 @@ Tip: On macOS, go into `System Preferences -> Notifications`, find the `terminal
 Once you have the dependencies set up, test the notifications by passing `test` in place of the state abbreviation:
 
     shotcaller test
-    # or
+    # or, when running from source
     yarn notify
+
+### SMS Notifications via Twilio
+
+Sign up for a [free trial Twilio account](www.twilio.com/referral/G6iTGO) (<- referral link) to enable SMS notifications (text messages) to your phone.
+
+If you are using a trial Twilio account, make sure you've verified the phone numbers you plan to send text messages to. You can do that [here](https://www.twilio.com/console/phone-numbers/verified).
+
+Once your account is setup, you must set the following required environment variables.
+
+When running shotcaller from source, simply add a `.env` file in the root of the cloned repository like so (example values):
+
+    # fake example values, get these from your Twilio account
+    TWILIO_FROM_NUMBER=+18021234567
+    TWILIO_ACCOUNT_SID=AC14758f1afd44c09b7992073ccf00b43d
+    TWILIO_AUTH_TOKEN=282b45b05d0eb361485b65ec88304ba5
+
+When running the `shotcaller` command, simply export the variables to your environment by running the following lines in your terminal or adding them to your `~/.zshrc` file:
+
+    # fake example values, get these from your Twilio account
+    export TWILIO_FROM_NUMBER=+18021234567
+    export TWILIO_ACCOUNT_SID=AC14758f1afd44c09b7992073ccf00b43d
+    export TWILIO_AUTH_TOKEN=282b45b05d0eb361485b65ec88304ba5
+
+To enable SMS notifications, first try a test run by passing in your phone number in the following format (substitute your own number):
+
+    shotcaller test +18027654321
+    # or
+    yarn notify +18027654321
+
+When you've sent a successful test, pass one or more phone numbers after the state abbreviation for the real deal:
+
+    shotcaller vt +18027654321 +18027654322 +18027654323
+    # or
+    yarn shotcaller vt +18027654321 +18027654322 +18027654323
 
 ---
 
 ## TODO
 
-- SMS/email notification, or other AFK alert medium
 - Add support for Walgreens and other providers
 - Figure out why `node-notifier` package doesn't work (plist error)
 - Improve how the npm module is packaged/published?
+- Add tests (lol)
--- src/cvs.js
+++ src/cvs.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { execSync } from "child_process";
+import Twilio from "twilio";
+import "./env";
 import {
   logDebug,
   logError,
@@ -9,10 +11,41 @@ import {
   sleepMinutes,
 } from "./utils";
 
-const { [2]: stateInput } = process.argv;
+const [_scriptPath, _scriptName, stateInput, ...phoneNumbers] = process.argv;
 if (!stateInput) {
   logErrorAndFail(`Must provide a US state abbreviation (i.e. yarn cvs VT)`);
 }
+let twilioClient;
+let twilioFromNumber;
+if (phoneNumbers.length > 0) {
+  process.env.TWILIO_ACCOUNT_SID;
+  process.env.TWILIO_AUTH_TOKEN;
+  twilioFromNumber = process.env.TWILIO_FROM_NUMBER;
+
+  if (
+    !twilioFromNumber ||
+    !process.env.TWILIO_ACCOUNT_SID ||
+    !process.env.TWILIO_AUTH_TOKEN
+  ) {
+    logErrorAndFail(
+      `Must provide the following environment variables when specifying phone numbers:\nTWILIO_ACCOUNT_SID\nTWILIO_AUTH_TOKEN\nTWILIO_FROM_NUMBER`
+    );
+  }
+
+  const invalidPhoneNumbers = phoneNumbers.filter(
+    (num) => !num.match(/^\+[1-9]\d{1,14}$/)
+  );
+  if (invalidPhoneNumbers.length > 0) {
+    logErrorAndFail(
+      `The following provided phone numbers are invalid. Format numbers with a leading '+' and a country code, e.g., +18021234567:\n${invalidPhoneNumbers.join(
+        "\n"
+      )}`
+    );
+  }
+
+  twilioClient = new Twilio();
+}
+
 const state = stateInput.toUpperCase();
 
 let terminalNotifierPath;
@@ -41,16 +74,22 @@ const request = {
   responseType: "json",
 };
 
-if (state === "TEST" && terminalNotifierPath) {
-  // Send a test notification
-  const notifyStatus = notify("FOOBAR", referer);
-  if (notifyStatus === 0) {
-    logSuccess(`No problems detected!`);
-  }
-  process.exit(notifyStatus);
-}
-
 async function run() {
+  if (state === "TEST" && terminalNotifierPath) {
+    // Send a test notification
+    const notifyStatus = await notify({
+      escapedAvailability: "FOOBAR",
+      callToActionUrl: referer,
+      phoneNumbers,
+      twilioFromNumber,
+      twilioClient,
+    });
+    if (notifyStatus === 0) {
+      logSuccess(`No problems detected!`);
+    }
+    process.exit(notifyStatus);
+  }
+
   const response = await axios(request);
 
   const {
@@ -93,28 +132,61 @@ async function run() {
       .join(", ");
 
     if (terminalNotifierPath) {
-      notify(escapedAvailability, referer);
+      await notify({
+        escapedAvailability,
+        callToActionUrl: referer,
+        phoneNumbers,
+        twilioFromNumber,
+        twilioClient,
+      });
     }
     logSuccess(
       `CVS has availability! In ${escapedAvailability}. Go now! ${referer}`
     );
   } else {
-    logInfo(`No availability as of ${dataUpdatedAtStr}`);
+    logInfo(`No availability (CVS data last updated at ${dataUpdatedAtStr})`);
   }
 }
 
-function notify(escapedAvailability, callToActionUrl) {
+async function notify({
+  escapedAvailability,
+  callToActionUrl,
+  phoneNumbers,
+  twilioFromNumber,
+  twilioClient,
+}) {
+  let localNotifyStatus = 0;
   try {
     execSync(
       `terminal-notifier -title "CVS has availability!" -subtitle "In ${escapedAvailability}" -message "Click to open page in browser now!" -open "${callToActionUrl}" -sound "Glass" -ignoreDnD`
     );
-    return 0;
   } catch (error) {
+    localNotifyStatus = error.status;
     logError(
       `Failed to send notification (status=${error.status} ${error.message})`
     );
-    return error.status;
   }
+
+  let twilioNotifyStatus = 0;
+  if (twilioClient && phoneNumbers.length > 0) {
+    logInfo(`Sending SMS notifications to ${phoneNumbers.length} numbers...`);
+    for (let index = 0; index < phoneNumbers.length; index++) {
+      const phoneNumber = phoneNumbers[index];
+      try {
+        const message = await twilioClient.messages.create({
+          body: `CVS has vaccine appointment availability at locations in: ${escapedAvailability}. ${callToActionUrl}`,
+          from: twilioFromNumber,
+          to: phoneNumber,
+        });
+        logInfo(`SMS sent to ${phoneNumber} [sid=${message.sid}]`);
+      } catch (error) {
+        logError(`Failed to send SMS to ${phoneNumber}. ${error.message}`);
+        twilioNotifyStatus = 1;
+      }
+    }
+  }
+
+  return localNotifyStatus === 0 && twilioNotifyStatus === 0 ? 0 : 1;
 }
 
 function poll() {
